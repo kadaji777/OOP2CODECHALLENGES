7d8512f302486021805f098eeb96c297f0b3cd62
kadaji777 <162417406+kadaji777@users.noreply.github.com>
2025-07-07 15:43:40 +0300

    Create ClearArray.java

---
+++ arrays/ClearArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+
+
+public class ClearArray{
+
+    public static void main(String[] args) {
+
+        String[] pallets = { "B14", "A11", "B12", "A13" };
+
+
+
+        System.out.println();
