7b56121e32da7adb3cd01cedb2e544a910a4b307
kadaji777 <162417406+kadaji777@users.noreply.github.com>
2025-07-11 16:50:23 +0300

    Create Product.java

---
+++ Single Responsibility Principle/Product.java
@@ -0,0 +1,21 @@
+//Single Responsibility Principle (SRP)
+//Code snippet adhering to this principle.
+
+//Product is only responsible for holding product data.
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
